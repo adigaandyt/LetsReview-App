c16f247404ea5fb98f95db1442e45f0c0d5c9ecb
adigaandyt <adigaandyt@gmail.com>
2024-02-21 00:21:16 +0200

    full e2e

--- e2e_test.sh
+++ e2e_test.sh
@@ -9,5 +9,5 @@ curl -X POST ${BASE_URL}/movies -H "Content-Type: application/json" -d '{"title"
 POST_RESPONSE=$(curl -X POST ${BASE_URL}/movies -H "Content-Type: application/json" -d '{"title": 123456}')
 echo $POST_RESPONSE
 MOVIE_ID=$(echo "$POST_RESPONSE" | jq -r '.movie_id')
-curl -X PUT ${BASE_URL}/movies/${MOVIE_ID} -H "Content-Type: application/json" -d '{"reviews": ["new review"]}'
+curl -X PUT ${BASE_URL}/movies/${MOVIE_ID} -H "Content-Type: application/json" -d '{review: "new review"}'
 curl -X DELETE ${BASE_URL}/movies/${MOVIE_ID} 
