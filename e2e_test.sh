868e1dc53f0a82a4642d1960b12c65a6a76a3755
adigaandyt <adigaandyt@gmail.com>
2024-02-21 00:12:22 +0200

    Post response test

--- e2e_test.sh
+++ e2e_test.sh
@@ -6,3 +6,7 @@ BASE_URL="http://nginx:80"
 curl -s "${BASE_URL}/health"
 curl -s "${BASE_URL}/movies"
 curl -X POST ${BASE_URL}/movies -H "Content-Type: application/json" -d '{"title": 123456}'
+POST_RESPONSE=$(curl -X POST ${BASE_URL}/movies -H "Content-Type: application/json" -d '{"title": 123456}')
+echo $POST_RESPONSE
+curl -X PUT ${BASE_URL}/movies -H "Content-Type: application/json" -d '{"title": 123456}'
+curl -X DELETE ${BASE_URL}/movies -H "Content-Type: application/json" -d '{"title": 123456}'
